97a240d5712132f987addaa0d124c6d6e47fcf9b
Art Art <144819984+lvl100boss@users.noreply.github.com>
2024-12-01 17:36:37 +0800

    Push New

--- admin/home.php
+++ admin/home.php
@@ -1,10 +1,54 @@
-<h1 class="">Welcome to <?php echo $_settings->info('name') ?> - Admin Side</h1>
+<h1 class="text-center text-light">Welcome to <?php echo $_settings->info('name') ?> - Admin Side</h1>
 <style>
-  #cover-image{
-    width:calc(100%);
-    height:50vh;
-    object-fit:cover;
-    object-position:center center;
+  body {
+    background-color: #121212;
+    color: #ffffff;
+  }
+
+  #cover-image {
+    width: 100%;
+    height: 50vh;
+    object-fit: cover;
+    object-position: center center;
+  }
+
+  .info-box {
+    background-color: #1e1e1e;
+    border-radius: 10px;
+    padding: 20px;
+    margin-bottom: 20px;
+  }
+
+  .info-box-icon {
+    background-color: #14453d !important;
+    border-radius: 50%;
+    padding: 15px;
+  }
+
+  .info-box-text,
+  .info-box-number {
+    color: #ffffff;
+  }
+
+  .info-box-number {
+    font-size: 1.5rem;
+  }
+
+  .clear-fix {
+    margin-bottom: 20px;
+  }
+
+  .text-center {
+    text-align: center;
+  }
+
+  .w-100 {
+    width: 100%;
+  }
+
+  a,
+  button {
+    color: #14453d;
   }
 </style>
 <hr>
@@ -12,114 +56,91 @@
   <div class="col-12 col-sm-4 col-md-4">
     <div class="info-box">
       <span class="info-box-icon bg-gradient-primary elevation-1"><i class="fas fa-th-list"></i></span>
-
       <div class="info-box-content">
         <span class="info-box-text">Total Categories</span>
-        <span class="iinfo-box-number text-right h4">
-          <?php 
-            $total = $conn->query("SELECT count(id) as total FROM category_list where delete_flag = 0 ")->fetch_assoc()['total'];
-            echo format_num($total);
+        <span class="info-box-number text-right h4">
+          <?php
+          $total = $conn->query("SELECT count(id) as total FROM category_list where delete_flag = 0 ")->fetch_assoc()['total'];
+          echo format_num($total);
           ?>
-          <?php ?>
         </span>
       </div>
-      <!-- /.info-box-content -->
     </div>
-    <!-- /.info-box -->
   </div>
   <div class="col-12 col-sm-4 col-md-4">
     <div class="info-box">
       <span class="info-box-icon bg-gradient-dark elevation-1"><i class="fas fa-th-list"></i></span>
-
       <div class="info-box-content">
         <span class="info-box-text">Total Shop Type</span>
-        <span class="iinfo-box-number text-right h4">
-          <?php 
-            $total = $conn->query("SELECT count(id) as total FROM shop_type_list where delete_flag = 0 ")->fetch_assoc()['total'];
-            echo format_num($total);
+        <span class="info-box-number text-right h4">
+          <?php
+          $total = $conn->query("SELECT count(id) as total FROM shop_type_list where delete_flag = 0 ")->fetch_assoc()['total'];
+          echo format_num($total);
           ?>
-          <?php ?>
         </span>
       </div>
-      <!-- /.info-box-content -->
     </div>
-    <!-- /.info-box -->
   </div>
   <div class="col-12 col-sm-4 col-md-4">
     <div class="info-box">
       <span class="info-box-icon bg-gradient-secondary elevation-1"><i class="fas fa-boxes"></i></span>
-
       <div class="info-box-content">
         <span class="info-box-text">Total Products</span>
-        <span class="iinfo-box-number text-right h4">
-          <?php 
-            $total = $conn->query("SELECT count(id) as total FROM product_list where delete_flag = 0 ")->fetch_assoc()['total'];
-            echo format_num($total);
+        <span class="info-box-number text-right h4">
+          <?php
+          $total = $conn->query("SELECT count(id) as total FROM product_list where delete_flag = 0 ")->fetch_assoc()['total'];
+          echo format_num($total);
           ?>
-          <?php ?>
         </span>
       </div>
-      <!-- /.info-box-content -->
     </div>
-    <!-- /.info-box -->
   </div>
   <div class="col-12 col-sm-4 col-md-4">
     <div class="info-box">
       <span class="info-box-icon bg-gradient-light border elevation-1"><i class="fas fa-users"></i></span>
-
       <div class="info-box-content">
         <span class="info-box-text">Vendors</span>
-        <span class="iinfo-box-number text-right h4">
-          <?php 
-            $total = $conn->query("SELECT count(id) as total FROM vendor_list where delete_flag = 0 ")->fetch_assoc()['total'];
-            echo format_num($total);
+        <span class="info-box-number text-right h4">
+          <?php
+          $total = $conn->query("SELECT count(id) as total FROM vendor_list where delete_flag = 0 ")->fetch_assoc()['total'];
+          echo format_num($total);
           ?>
-          <?php ?>
         </span>
       </div>
-      <!-- /.info-box-content -->
     </div>
-    <!-- /.info-box -->
-  </div><div class="col-12 col-sm-4 col-md-4">
+  </div>
+  <div class="col-12 col-sm-4 col-md-4">
     <div class="info-box">
       <span class="info-box-icon bg-gradient-maroon elevation-1"><i class="fas fa-user-friends"></i></span>
-
       <div class="info-box-content">
         <span class="info-box-text">Clients</span>
-        <span class="iinfo-box-number text-right h4">
-          <?php 
-            $total = $conn->query("SELECT count(id) as total FROM client_list where delete_flag = 0 ")->fetch_assoc()['total'];
-            echo format_num($total);
+        <span class="info-box-number text-right h4">
+          <?php
+          $total = $conn->query("SELECT count(id) as total FROM client_list where delete_flag = 0 ")->fetch_assoc()['total'];
+          echo format_num($total);
           ?>
-          <?php ?>
         </span>
       </div>
-      <!-- /.info-box-content -->
     </div>
-    <!-- /.info-box -->
   </div>
   <div class="col-12 col-sm-4 col-md-4">
     <div class="info-box">
       <span class="info-box-icon bg-gradient-warning elevation-1"><i class="fas fa-list"></i></span>
-
       <div class="info-box-content">
         <span class="info-box-text">Total Pending Orders</span>
-        <span class="iinfo-box-number text-right h4">
-          <?php 
-            $total = $conn->query("SELECT count(id) as total FROM order_list where `status` = 0 ")->fetch_assoc()['total'];
-            echo format_num($total);
+        <span class="info-box-number text-right h4">
+          <?php
+          $total = $conn->query("SELECT count(id) as total FROM order_list where `status` = 0 ")->fetch_assoc()['total'];
+          echo format_num($total);
           ?>
-          <?php ?>
         </span>
       </div>
-      <!-- /.info-box-content -->
     </div>
-    <!-- /.info-box -->
   </div>
 </div>
 
 <div class="clear-fix mb-2">
-    <div class="text-center w-100">
-      <img src="<?= validate_image($_settings->info('cover')) ?>" alt="System Cover image" class="w-100" id="cover-image">
-    </div>
+  <div class="text-center w-100">
+    <img src="<?= validate_image($_settings->info('cover')) ?>" alt="System Cover image" class="w-100" id="cover-image">
   </div>
+</div>
--- vendor/products/index.php
+++ vendor/products/index.php
@@ -1,27 +1,106 @@
-<?php if($_settings->chk_flashdata('success')): ?>
-<script>
-	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
-</script>
-<?php endif;?>
+<?php if ($_settings->chk_flashdata('success')): ?>
+	<script>
+		alert_toast("<?php echo $_settings->flashdata('success') ?>", 'success')
+	</script>
+<?php endif; ?>
 <style>
-	.product-img{
+	body {
+		background-color: #121212;
+		color: #e0e0e0;
+	}
+
+	.card {
+		background-color: #1e1e1e;
+		border: 1px solid #14453d;
+	}
+
+	.card-header {
+		background-color: #14453d;
+		color: #ffffff;
+	}
+
+	.card-title {
+		color: #ffffff;
+	}
+
+	.card-tools .btn {
+		background-color: #14453d;
+		color: #ffffff;
+	}
+
+	.table {
+		color: #e0e0e0;
+	}
+
+	.table th,
+	.table td {
+		border-color: #14453d;
+	}
+
+	.table thead {
+		background-color: #14453d;
+	}
+
+	.table thead th {
+		color: #ffffff;
+	}
+
+	.table tbody tr {
+		background-color: #1e1e1e;
+	}
+
+	.table tbody tr:hover {
+		background-color: #2c2c2c;
+	}
+
+	.product-img {
 		width: calc(100%);
 		height: auto;
 		max-width: 5em;
-		object-fit:scale-down;
-		object-position:center center;
+		object-fit: scale-down;
+		object-position: center center;
+	}
+
+	.btn-flat {
+		background-color: #14453d;
+		color: #ffffff;
+	}
+
+	.btn-flat:hover {
+		background-color: #0f3a32;
+	}
+
+	.dropdown-menu {
+		background-color: #1e1e1e;
+		border: 1px solid #14453d;
+	}
+
+	.dropdown-item {
+		color: #e0e0e0;
+	}
+
+	.dropdown-item:hover {
+		background-color: #14453d;
+		color: #ffffff;
+	}
+
+	.badge-success {
+		background-color: #14453d;
+	}
+
+	.badge-danger {
+		background-color: #b71c1c;
 	}
 </style>
-<div class="card card-outline card-primary">
+<div class="card card-outline card-primary border-0">
 	<div class="card-header">
 		<h3 class="card-title">List of Products</h3>
 		<div class="card-tools">
-			<a href="javascript:void(0)" class="btn btn-flat btn-primary" id="create_new"><span class="fas fa-plus"></span>  Create New</a>
+			<a href="javascript:void(0)" class="btn btn-flat btn-primary" id="create_new"><span class="fas fa-plus"></span> Create New</a>
 		</div>
 	</div>
-	<div class="card-body">
-		<div class="container-fluid">
-        <div class="container-fluid">
+	<div class="card-body bg-black">
+		<div class="container-fluid bg-black">
 			<table class="table table-bordered table-stripped">
 				<colgroup>
 					<col width="5%">
@@ -33,7 +112,7 @@
 					<col width="15%">
 				</colgroup>
 				<thead>
-					<tr class="bg-gradient-secondary">
+					<tr class="bg-black">
 						<th>#</th>
 						<th>Date Created</th>
 						<th>Image</th>
@@ -44,79 +123,81 @@
 					</tr>
 				</thead>
 				<tbody>
-					<?php 
+					<?php
 					$i = 1;
-						$qry = $conn->query("SELECT * from `product_list` where delete_flag = 0 and `vendor_id` = '{$_settings->userdata('id')}' order by `name` asc ");
-						while($row = $qry->fetch_assoc()):
+					$qry = $conn->query("SELECT * from `product_list` where delete_flag = 0 and `vendor_id` = '{$_settings->userdata('id')}' order by `name` asc ");
+					while ($row = $qry->fetch_assoc()):
 					?>
 						<tr>
 							<td class="text-center"><?php echo $i++; ?></td>
-							<td><?php echo date("Y-m-d H:i",strtotime($row['date_created'])) ?></td>
+							<td><?php echo date("Y-m-d H:i", strtotime($row['date_created'])) ?></td>
 							<td class="text-center"><img src="<?= validate_image($row['image_path']) ?>" alt="Product Image" class="border border-gray img-thumbnail product-img"></td>
 							<td><?php echo $row['name'] ?></td>
 							<td class="text-right"><?php echo format_num($row['price']) ?></td>
 							<td class="text-center">
-                                <?php if($row['status'] == 1): ?>
-                                    <span class="badge badge-success bg-gradient-success px-3 rounded-pill">Active</span>
-                                <?php else: ?>
-                                    <span class="badge badge-danger bg-gradient-danger px-3 rounded-pill">Inactive</span>
-                                <?php endif; ?>
-                            </td>
+								<?php if ($row['status'] == 1): ?>
+									<span class="badge badge-success bg-gradient-success px-3 rounded-pill">Active</span>
+								<?php else: ?>
+									<span class="badge badge-danger bg-gradient-danger px-3 rounded-pill">Inactive</span>
+								<?php endif; ?>
+							</td>
 							<td align="center">
-								 <button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
-				                  		Action
-				                    <span class="sr-only">Toggle Dropdown</span>
-				                  </button>
-				                  <div class="dropdown-menu" role="menu">
-				                    <a class="dropdown-item view_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-eye text-dark"></span> View</a>
-				                    <div class="dropdown-divider"></div>
-				                    <a class="dropdown-item edit_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-edit text-primary"></span> Edit</a>
-				                    <div class="dropdown-divider"></div>
-				                    <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
-				                  </div>
+								<button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
+									Action
+									<span class="sr-only">Toggle Dropdown</span>
+								</button>
+								<div class="dropdown-menu" role="menu">
+									<a class="dropdown-item view_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-eye text-dark"></span> View</a>
+									<div class="dropdown-divider"></div>
+									<a class="dropdown-item edit_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-edit text-primary"></span> Edit</a>
+									<div class="dropdown-divider"></div>
+									<a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
+								</div>
 							</td>
 						</tr>
 					<?php endwhile; ?>
 				</tbody>
 			</table>
 		</div>
-		</div>
 	</div>
 </div>
 <script>
-	$(document).ready(function(){
-		$('#create_new').click(function(){
-			uni_modal('Add New Product',"products/manage_product.php",'large')
+	$(document).ready(function() {
+		$('#create_new').click(function() {
+			uni_modal('Add New Product', "products/manage_product.php", 'large')
 		})
-		$('.view_data').click(function(){
-			uni_modal('View Product Details',"products/view_product.php?id="+$(this).attr('data-id'),'large')
+		$('.view_data').click(function() {
+			uni_modal('View Product Details', "products/view_product.php?id=" + $(this).attr('data-id'), 'large')
 		})
-		$('.edit_data').click(function(){
-			uni_modal('Update Product',"products/manage_product.php?id="+$(this).attr('data-id'),'large')
+		$('.edit_data').click(function() {
+			uni_modal('Update Product', "products/manage_product.php?id=" + $(this).attr('data-id'), 'large')
 		})
-		$('.delete_data').click(function(){
-			_conf("Are you sure to delete this product permanently?","delete_product",[$(this).attr('data-id')])
+		$('.delete_data').click(function() {
+			_conf("Are you sure to delete this product permanently?", "delete_product", [$(this).attr('data-id')])
 		})
 		$('table th,table td').addClass('align-middle px-2 py-1')
 		$('.table').dataTable();
 	})
-	function delete_product($id){
+
+	function delete_product($id) {
 		start_loader();
 		$.ajax({
-			url:_base_url_+"classes/Master.php?f=delete_product",
-			method:"POST",
-			data:{id: $id},
-			dataType:"json",
-			error:err=>{
+			url: _base_url_ + "classes/Master.php?f=delete_product",
+			method: "POST",
+			data: {
+				id: $id
+			},
+			dataType: "json",
+			error: err => {
 				console.log(err)
-				alert_toast("An error occured.",'error');
+				alert_toast("An error occured.", 'error');
 				end_loader();
 			},
-			success:function(resp){
-				if(typeof resp== 'object' && resp.status == 'success'){
+			success: function(resp) {
+				if (typeof resp == 'object' && resp.status == 'success') {
 					location.reload();
-				}else{
-					alert_toast("An error occured.",'error');
+				} else {
+					alert_toast("An error occured.", 'error');
 					end_loader();
 				}
 			}
